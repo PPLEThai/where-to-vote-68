1234228ff0e4985f4b8af642973021a9ba527d5c
PanJ <panjmp@gmail.com>
2025-01-29 23:32:26 +0700

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+// import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import YourPen from "./YourPen.tsx";
+// import YourPen from "./YourPen.tsx";
 import Static from "./Static.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -11,7 +11,7 @@ createRoot(document.getElementById("root")!).render(
       <Routes>
         {/* <Route path="/" element={<App />} /> */}
         <Route path="/" element={<Static />} />
-        <Route path="/" element={<YourPen />} />
+        {/* <Route path="/" element={<YourPen />} /> */}
       </Routes>
     </BrowserRouter>
   </StrictMode>
